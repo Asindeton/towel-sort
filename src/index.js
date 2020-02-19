a82deea3fa303f2e6c495de51eb0a3e69d805828
Дмитрий Леганьков <you@example.com legankov95@gmail.comgit config --global user.name Asindetongit commit -m 'test cv'$ git config --global user.email you@example.com>
2020-02-19 22:51:31 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+const towelSort = (matrix = []) => {
+    const prepared_matrix = matrix.map((elem, i) =>
+        i % 2 !== 0 ? elem.reverse() : elem
+    );
+    return [].concat.apply([], prepared_matrix);
+};
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = towelSort;
